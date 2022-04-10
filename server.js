abe100b2742b435d131af8b7d1709c49e86c787b
Wooow25 <regnerischmorgen@gmail.com>
2022-04-11 00:26:48 +0300

    test create query and create delay function

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const dbOperations = require('./dbFiles/operations')
+const {Movie} = require("./dbFiles/classes");
+
+
+function delay(timeout) {
+    return new Promise(() => {
+        setTimeout(() => {}, timeout);
+    });
+}
 
 // const API_PORT = process.env.PORT || 5000;
 // const app = express(); //command to start server
@@ -22,6 +30,15 @@ const dbOperations = require('./dbFiles/operations')
 //
 // app.listen(API_PORT, () => console.log(`listening on ${API_PORT}`))
 
+let cart= new Movie(18,'doc',148,'Peace', '')
+
+
+ dbOperations.createMovie(cart).then(res=>{
+    console.log(res.recordset)
+})
+
+
 dbOperations.getMovies().then(res=>{
     console.log(res.recordset)
 })
+
